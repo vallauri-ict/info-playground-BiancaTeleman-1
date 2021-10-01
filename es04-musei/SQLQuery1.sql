a67a138d30629770de3ffbabfb53d9c8f450fcac
Stud. Bianca Elena Teleman <b.teleman.1387@vallauri.net>
2021-10-01 11:01:46 +0200

    did query n8 andn9

---
+++ es04-musei/SQLQuery1.sql
@@ -0,0 +1,16 @@
+﻿/*Per ciascun museo di Londra, il numero di opere di artisti italiani ivi conservate*/
+SELECT m.NomeM,count(*) as NumeroOpere
+FROM Artisti a,Opere o, Musei m
+WHERE a.Nazionalità='IT'
+AND a.NomeA=o.NomeA AND m.NomeM=o.NomeM
+AND m.Città='Londra'
+GROUP BY m.NomeM
+
+/*Il nome dei musei di londra che non hanno opere di Tiziano*/
+SELECT m.NomeM
+FROM Musei m
+WHERE m.Città='Londra'
+AND NOT EXISTS(SELECT * 
+			   FROM Opere o 
+			   WHERE o.NomeA='Tiziano'
+			   AND o.NomeM=m.NomeM)  /*devo collegarmi alla query esterna*/
